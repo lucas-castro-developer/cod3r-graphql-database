378a77c4abee30ace5fc9c1226174fb60ba29204
Lucas Castro <lucas.sc98@hotmail.com>
2020-12-10 20:49:17 -0300

    Migration #02 - seed example

--- migrations/20201208211038_tabela_perfis.js
+++ migrations/20201208211038_tabela_perfis.js
@@ -1,11 +1,19 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("perfis", (table) => {
-    table.increments("id").primary();
-    table.string("nome").notNull().unique();
-    table.string("rotulo").notNull();
-  });
+  return knex.schema
+    .createTable("perfis", (table) => {
+      table.increments("id").primary();
+      table.string("nome").notNull().unique();
+      table.string("rotulo").notNull();
+    })
+    .then(function () {
+      return knex("perfis").insert([
+        { nome: "comum", rotulo: "Comum" },
+        { nome: "admin", rotulo: "Administrador" },
+        { nome: "master", rotulo: "Master" },
+      ]);
+    });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTable('perfis')
+  return knex.schema.dropTable("perfis");
 };
